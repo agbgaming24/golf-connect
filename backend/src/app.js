ac2fcfe2cd45cfe76af70164e1767f6bb921245e
agbgaming24 <aryan2446.pis@gmail.com>
2026-03-26 16:04:42 +0530

    Fix Express 5 CORS preflight wildcard route

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/draws', require('./routes/draw.routes'));
